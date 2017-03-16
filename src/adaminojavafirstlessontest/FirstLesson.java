d143113dd046600b769d51a1b5620fa7f57d1f0f
Adam Hansen <adamlars90@gmail.com>
2017-03-16 16:31:45 +0100

    Updated assignments

--- src/adaminojavafirstlessontest/FirstLesson.java
+++ src/adaminojavafirstlessontest/FirstLesson.java
@@ -5,22 +5,29 @@
  */
 package adaminojavafirstlessontest;
 
-public class Test {
+public class FirstLesson {
 
     //Create field to hold studentFirstName, initialize it to your first name
     //Create field to hold the value isLearning, initialize it to true
     //Create field to hold an int called currentSemester, initialize it to 2
-    //Create constant field to hold a name called TEACHER, initialize to Adam
-    public Test() {
+    //Create a public constant field to hold a name called TEACHER, initialize to Adam
+    public FirstLesson() {
     }
 
     //Create a constructor which can studentFirstName as argument and sets the value of isLearning to false and currentSemester to 1
-    //Create a method to print studentName to the console
-    //Create a method to print studentName if the value isLearning is true
+    //Create a method to print studentName to the console called printStudentName
+    //Create a method to print studentName if the value isLearning is true, call it printLearningStudent
     /*Create a method that takes an int "semester" as argument
     and returns studentName in case the int equals 2 and returns an empty value in case semester is 1, 3, 4, 5 or default
+    Call it returnOnSecondSemester
      */
+ /*Create an int Array field which contains numbers from 1 to 5,
+call it arrayOfSemesters
+remember to create a getter for it afterwards!
+     */
+ /*Create an Integer ArrayList field that also contains the numbers from 1 to 5,
+call it arrayListOfSemesters
+remember to create a getter for it afterwards*/
+    //Create a method to set the value isLearning to a new value
     //Create methods to get all the fields from this class
-    //Create an int Array which contains numbers from 1 to 5, call it arrayOfSemesters
-    //Create an Integer ArrayList that also contains the numbers from 1 to 5, call it arrayListOfSemesters
 }
