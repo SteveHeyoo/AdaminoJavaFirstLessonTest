1cd3b0c10a922c701290aa31bd455ea5fa1b3c60
Adam Hansen <adamlars90@gmail.com>
2017-03-28 19:16:11 +0200

    Added test assignments for Calculator

---
+++ test/logicToTest/CalculatorTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logicToTest;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author gta1
+ */
+public class CalculatorTest {
+
+    public CalculatorTest() {
+    }
+
+    //Write a test to check if the Calculator can do addition
+    @Test
+    public void testAdd() {
+    }
+
+    //Write a test to check if the Calculator can do substraction
+    @Test
+    public void testSubstract() {
+    }
+
+    //Write a test to check if the Calculator can do division
+    @Test
+    public void testDivide() {
+    }
+
+    //Write a test to check if the Calculator can do multiplication
+    @Test
+    public void testMultiplicate() {
+    }
+
+}
