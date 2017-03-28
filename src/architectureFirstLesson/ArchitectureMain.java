1a0af774680a51392066a1b0d485398373e15456
Adam Hansen <adamlars90@gmail.com>
2017-03-28 18:29:04 +0200

    Added architecture to project

---
+++ src/architectureFirstLesson/ArchitectureMain.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package architectureFirstLesson;
+
+public class ArchitectureMain {
+
+    public static void main(String[] args) {
+        /*
+         * 1. Create new model called AnimalModel
+         *
+         * 2. This model should be able to hold a collection of animals
+         * We need at least a Cat and a Dog!
+         *
+         * 3. The animals should implement the interface called IAnimalActions
+         *
+         * 4. Create a mock AnimalDAO which returns a collection of Cats and
+         * Dogs
+         * to the model when it is created!
+         */
+
+        //Instatiate a new AnimalModel here
+        //For each animal in the models collection called animals, call makeSound();
+        //Make sure that all files are put in the correct folder according to 3-tier architecture, like IAnimalActions
+    }
+
+}
---
+++ src/architectureFirstLesson/bll/IAnimalActions.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package architectureFirstLesson.bll;
+
+/**
+ *
+ * @author gta1
+ */
+public interface IAnimalActions {
+
+    public void makeSound();
+
+}
