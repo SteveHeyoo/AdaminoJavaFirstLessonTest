a118251fbac3d2316f973f748616c535597d4fba
SteveHeyoo <bosinclair32@gmail.com>
2017-05-08 14:33:19 +0200

    Second Lession - Switch Statments

--- src/adaminojavafirstlessontest/FirstLesson.java
+++ src/adaminojavafirstlessontest/FirstLesson.java
@@ -12,7 +12,7 @@ public class FirstLesson
     private String studentFirstName;
     private boolean isLearning;
     private int currentSemester;
-    public final String TEACHER = "Adam";//Constant
+    public static final String TEACHER = "Adam";//Constant, public static final, helps other programmers identify. Access it via .teacher, we use less memory via using a static variable.
     
     
     //Create field to hold studentFirstName, initialize it to your first name
@@ -21,7 +21,7 @@ public class FirstLesson
     //Create a public constant field to hold a name called TEACHER, initialize to Adam
     
     
-    //Method overloading, if you make more methods with the same name
+    
     public FirstLesson() 
     {
         studentFirstName = "Bo";//instantiated the variables
@@ -31,9 +31,10 @@ public class FirstLesson
         
         
     }
+    
     //Create a constructor which can studentFirstName as argument and sets the value of isLearning to false and currentSemester to 1
     //Brackets give you the option to method overload, via providing parameters
-    public FirstLesson(String name)
+    public FirstLesson(String name) //Method overloading, if you make more methods with the same name (Polymorphism)
     {
         studentFirstName = name; //Dependancy injection, flexability, which means I can make whatever names I want.
         isLearning = false;
@@ -44,9 +45,10 @@ public class FirstLesson
 
     
     //Create a method to print studentName to the console called printStudentName
-    public void printStudentName()//method declaration, void doesn't return anything
+    
+    public void printStudentName()//method declaration, void doesn't return anything, otherwise you'd need a return method "return; blah blah"      
     {
-        String name = "bo";//Local variable
+        String name = "bo";//Local variable, only exsists when we run this method
         System.out.println(studentFirstName);
     }
     
@@ -64,10 +66,23 @@ public class FirstLesson
     /*Create a method that takes an int "semester" as argument
     and returns studentName in case the int equals 2 and returns an empty value in case semester is 1, 3, 4, 5 or default
     Call it returnOnSecondSemester
-     */
- /*Create an int Array field which contains numbers from 1 to 5,
-call it arrayOfSemesters
-remember to create a getter for it afterwards!
+    */
+    public String returnOnSecondSemester(int semester)//Has an int as an arguement and returns a string.
+    {
+        switch (semester)//Condition, what it validates between your brackets //Switch statement, better than if or else statements, but only if the method you are creating has millions of requirements for if or else.
+        { 
+            case 2:
+                return studentFirstName; //Remember to either return or break for a case, else you'll recieve a fall through.
+            default:
+                return ""; //Are always required a default value.
+        }
+        
+    }
+    
+    
+    /*Create an int Array field which contains numbers from 1 to 5,
+    call it arrayOfSemesters
+    remember to create a getter for it afterwards!
      */
  /*Create an Integer ArrayList field that also contains the numbers from 1 to 5,
 call it arrayListOfSemesters
